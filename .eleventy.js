ce1487483922229d6f167b4204fc1d9531ef9375
Kyle Hall <kyle.d.hall@accenture.com>
2020-01-03 09:53:33 -0600

    add syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,8 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
+
+module.exports = function(config) {
+  config.addPassthroughCopy('css/')
+  config.addPassthroughCopy({ 'node_modules/prism-themes/themes/prism-atom-dark.css': 'css/lib/prism-theme.css' })
+
+  config.addPlugin(syntaxHighlight)
 }
---
+++ posts/a-post-with-code.md
@@ -0,0 +1,10 @@
+---
+layout: layouts/mainLayout.html
+title: A Post With Code
+---
+
+# {{ title }}
+
+```js
+const food = 'spaghetti'
+```
